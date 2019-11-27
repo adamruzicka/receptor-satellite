1fbeb9acce4a992a1f6281fb3c6cf550171dfd22
Adam Ruzicka <aruzicka@redhat.com>
2019-11-27 13:58:18 +0000

    Implement cert verification

--- receptor_satellite/satellite_api.py
+++ receptor_satellite/satellite_api.py
@@ -1,5 +1,6 @@
 import aiohttp
 import json
+import ssl
 
 
 class SatelliteAPI:
@@ -7,6 +8,13 @@ class SatelliteAPI:
         self.username = username
         self.password = password
         self.url = url
+        self.context = None
+        if url.startswith('https'):
+            self.context = ssl.SSLContext()
+            cafile = '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
+            self.context.load_verify_locations(cafile=cafile)
+            self.context.verify_mode = ssl.CERT_REQUIRED
+
 
     @classmethod
     def from_plugin_config(cls, plugin_config):
@@ -25,10 +33,9 @@ class SatelliteAPI:
         url = f'{self.url}/api/v2/job_invocations'
         extra_data = {
             "json": payload,
-            "headers": {"Content-Type": "application/json"},
-            "auth": aiohttp.BasicAuth(self.username, self.password)
+            "headers": {"Content-Type": "application/json"}
         }
-        response = await request('POST', url, extra_data)
+        response = await self.request('POST', url, extra_data)
         return sanitize_response(response, 201)
 
 
@@ -37,22 +44,24 @@ class SatelliteAPI:
         extra_data = {"auth": aiohttp.BasicAuth(self.username, self.password)}
         if since is not None:
             extra_data["params"] = {"since": str(since)}
-        response = await request('GET', url, extra_data)
+        response = await self.request('GET', url, extra_data)
         return sanitize_response(response, 200)
 
 
-async def request(method, url, extra_data):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.request(method, url, **extra_data) as response:
-                return dict(status=response.status,
-                            body=await response.text(),
-                            error=None)
-    except Exception as e:
-        return dict(error=e, body="{}", status=-1)
+    async def request(self, method, url, extra_data):
+        try:
+            async with aiohttp.ClientSession() as session:
+                extra_data = {**extra_data, **dict(auth=aiohttp.BasicAuth(self.username, self.password), ssl=self.context)}
+                async with session.request(method, url, **extra_data) as response:
+                    return dict(status=response.status,
+                                body=await response.text(),
+                                error=None)
+        except Exception as e:
+            return dict(error=e, body="{}", status=-1)
 
 
 def sanitize_response(response, expected_status):
+    print(response)
     if not response['error']:
         response['body'] = json.loads(response['body'])
         if response['status'] != expected_status:
