4be44f141fa9761a4e6b5d085fb37ca20e5cf399
Adam Ruzicka <aruzicka@redhat.com>
2020-02-18 09:16:01 +0100

    Drop pointless returns

--- receptor_satellite/response_queue.py
+++ receptor_satellite/response_queue.py
@@ -3,24 +3,25 @@ class ResponseQueue:
         self.queue = queue
 
     def ack(self, playbook_run_id):
-        payload = {"type": "playbook_run_ack", "playbook_run_id": playbook_run_id}
-        return self.queue.put(payload)
+        self.queue.put({"type": "playbook_run_ack", "playbook_run_id": playbook_run_id})
 
     def playbook_run_update(self, host, playbook_run_id, output, sequence):
-        payload = {
-            "type": "playbook_run_update",
-            "playbook_run_id": playbook_run_id,
-            "sequence": sequence,
-            "host": host,
-            "console": output,
-        }
-        return self.queue.put(payload)
+        self.queue.put(
+            {
+                "type": "playbook_run_update",
+                "playbook_run_id": playbook_run_id,
+                "sequence": sequence,
+                "host": host,
+                "console": output,
+            }
+        )
 
     def playbook_run_finished(self, host, playbook_run_id, success=True):
-        payload = {
-            "type": "playbook_run_finished",
-            "playbook_run_id": playbook_run_id,
-            "host": host,
-            "status": "success" if success else "failure",
-        }
-        return self.queue.put(payload)
+        self.queue.put(
+            {
+                "type": "playbook_run_finished",
+                "playbook_run_id": playbook_run_id,
+                "host": host,
+                "status": "success" if success else "failure",
+            }
+        )
